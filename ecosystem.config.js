a8904def1c6c777b6fa7d0332141f27fe3eb4dc5
Dimantha Kaveen <vpdkkaveen@gmail.com>
2026-08-25 18:33:42 +0530

    fix: update PM2 ecosystem to run java processes with prod profile

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,25 +1,22 @@
-module.exports = {
+﻿module.exports = {
   apps: [
     {
       name: "config-server",
-      script: "java.exe",
+      script: "java",
       args: "-jar config-server/target/Config-Server-1.0.0.jar",
-      log_file: "./logs/config-server.log",
-      env: { SPRING_PROFILES_ACTIVE: 'native' }
+      log_file: "./logs/config-server.log"
     },
     {
       name: "service-registry",
-      script: "java.exe",
+      script: "java",
       args: "-jar service-registry/target/Service-Registry-1.0.0.jar",
-      log_file: "./logs/service-registry.log",
-      env: { SPRING_PROFILES_ACTIVE: 'dev', SPRING_CLOUD_CONFIG_URI: 'http://localhost:9000' }
+      log_file: "./logs/service-registry.log"
     },
     {
       name: "api-gateway",
-      script: "java.exe",
-      args: "-jar api-gateway/target/Api-Gateway-1.0.0.jar",
-      log_file: "./logs/api-gateway.log",
-      env: { SPRING_PROFILES_ACTIVE: 'dev', SPRING_CLOUD_CONFIG_URI: 'http://localhost:9000' }
+      script: "java",
+      args: "-jar api-gateway/target/Api-Gateway-1.0.0.jar --spring.profiles.active=prod",
+      log_file: "./logs/api-gateway.log"
     }
   ]
-}
+};
